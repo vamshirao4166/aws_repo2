9ba9286301e3126b545fe8e2139551fec1a9f972
Vamshi Rao <vamshirao4166@gmail.com>
2025-07-03 14:15:02 +0530

    added subnet

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
+}
